42a2ea9e43ae9a2f628b8f216f0adaeb5085303d
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2024-01-10 19:01:27 -0500

    exporting data

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb'; // imports the openDB function from idb, which is used to open the IndexedDB database
 
-const initdb = async () => // creates the database if it doesn't exist
+const initdb = async () =>  // creates the database if it doesn't exist
   // open the database
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +13,25 @@ const initdb = async () => // creates the database if it doesn't exist
     },
   });
 
-export const putDb = async (content) => console.error('putDb not implemented'); // putDB is from database.js, it adds content to the database.
+export const putDb = async (content) =>{
+  const jateDb = await openDB('jate', 1); // open the database
+  const tx = jateDb.transaction('jate', 'readwrite'); // start a transaction
+  const store = tx.objectStore('jate'); // get the 'jate' object store
+  const request = store.put({ jate: content }); // put the content in the 'jate' object store
+  const result = await request;
+  console.log('data saved to the database', result); // get the result
+};
+console.error('putDb not implemented'); // putDB is from database.js, it adds content to the database.
 
-export const getDb = async () => console.error('getDb not implemented'); 
+export const getDb = async () =>{
+  const jateDb = await openDB('jate', 1); // open the database
+  const tx = jateDb.transaction('jate', 'readonly'); // start a transaction
+  const store = tx.objectStore('jate'); // get the 'jate' object store
+  const request = store.getAll(); // get all the content in the 'jate' object store
+  const result = await request; // get the result
+  return result?.jate || 'Database is empty'; // return the result 
+};
+
 // getDB is from database.js, it gets all the content from the database and returns it as a promise that resolves to a string, 
 // which is the content of the database, or an empty string if the database is empty, or an error if something goes wrong.
 
